01491ff75b5db617d47d1b8fd45da3931182642d
Aitor <aitorchain@gmail.com>
2026-04-14 23:43:24 +0200

    fix cors error

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -15,7 +15,12 @@ async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule, {
     logger: ["log", "error", "warn"],
   });
-  app.enableCors();
+  app.enableCors({
+    origin: ["https://carfinder-eta.vercel.app"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
